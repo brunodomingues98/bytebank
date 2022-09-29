e20395b4b8a0d88d47d30a342a49c9db954a6cf4
Bruno Domingues <p.brunodomingues@gmail.com>
2022-09-29 15:53:42 -0300

    comunicacaoHTTP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 import { FormsModule } from '@angular/forms';
-import { ExtratoComponent } from './extrato/extrato.component';
-import { registerLocaleData } from '@angular/common';
-import localtePt from '@angular/common/locales/pt';
-
-registerLocaleData(localtePt, 'pt');
 
 @NgModule({
   declarations: [
     AppComponent,
-    NovaTransferenciaComponent,
-    ExtratoComponent
+    NovaTransferenciaComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [
-    {provide: LOCALE_ID, useValue: 'pt'},
-    {
-      provide: DEFAULT_CURRENCY_CODE,
-      useValue: "BRL"
-    }
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
